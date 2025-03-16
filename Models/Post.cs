b0170d9d7b3d9d8441d563c83477217c0e55a7ea
Melika <89937665+melikazn@users.noreply.github.com>
2025-03-16 19:48:43 +0330

    Add files via upload

---
+++ Models/Post.cs
@@ -0,0 +1,12 @@
+namespace MyBlazor.Models
+{
+    public class Post
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string published { get; set; }
+        public string content { get; set; }
+
+    }
+}
